00659b71989c0d1d14116a2c2fe5064bbaeb3139
Brody Couture <brodycouture05@gmail.com>
2025-10-28 07:41:07 -0800

    Finished exercise 5

--- Week5/Exercise5/main.rb
+++ Week5/Exercise5/main.rb
@@ -2,15 +2,14 @@
 
 class LaunchDiscussionWorkflow
 
-  def initialize(discussion, host, participants_email_string)
+  def initialize(discussion, host, participants)
     @discussion = discussion
     @host = host
-    @participants_email_string = participants_email_string
-    @participants = []
+    @participants = participants
   end
 
   # Expects @participants array to be filled with User objects
-  def run
+  def launch_discussion
     return unless valid?
     run_callbacks(:create) do
       ActiveRecord::Base.transaction do
@@ -21,25 +20,42 @@ class LaunchDiscussionWorkflow
     end
   end
 
-  def generate_participant_users_from_email_string
-    return if @participants_email_string.blank?
-    @participants_email_string.split.uniq.map do |email_address|
-      User.create(email: email_address.downcase, password: Devise.friendly_token)
-    end
-  end
+  
 
   # ...
 
 end
 
+class ListOfParticipants
+  def initialize(email_string)
+    @email_string = parse_email_string(email_string)
+  end
 
+  # Adds users created with emails from the email string to the participants array, and returns the full list of participants
+  def generate_participant_users_from_email_string
+    participants = []
+    @email_string do |email_address|
+      @participants.append(User.create(email: email_address.downcase, password: Devise.friendly_token))
+    end
+    participants
+  end
+
+  # Splits up the email string into a list of email addresses
+  def parse_email_string(email_string)
+    return [] if email_string.blank?
+    email_string = email_string.split.uniq.map
+  end
+end
+
+email_string = "fake1@example.com\nfake2@example.com\nfake3@example.com"
+
+# Creates the components we need to launch our discussion
 discussion = Discussion.new(title: "fake", ...)
 host = User.find(42)
-participants = "fake1@example.com\nfake2@example.com\nfake3@example.com"
+participants = ListOfParticipants.new(email_string).generate_participant_users_from_email_string
 
 workflow = LaunchDiscussionWorkflow.new(discussion, host, participants)
-workflow.generate_participant_users_from_email_string
-workflow.run
+workflow.launch_discussion
 
 
 
