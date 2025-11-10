54f903bef455d372b2a4de67cdb879e3b7bf515b
Brody Couture <brodycouture05@gmail.com>
2025-11-10 14:29:24 -0800

    finished challenge 7.2

---
+++ Week7/in_class_7_2.rb
@@ -0,0 +1,52 @@
+# Protects against change!!!!!!!
+def my_wrapped_dependency(a, b, c)
+    third_party_dependency_that_changes_very_frequently(b, a, c)
+end
+
+def myfunction1(a, b, c)
+  return my_wrapped_dependency(a, b, c)
+end
+
+def myfunction2(a, b, c)
+  return my_wrapped_dependency(a, b, c)
+end
+
+def myfunction3(a, b, c)
+  return my_wrapped_dependency(a, b, c)
+end
+
+def myfunction4(a, b, c)
+  return my_wrapped_dependency(a, b, c)
+end
+
+def myfunction5(a, b, c)
+  return my_wrapped_dependency(a, b, c)
+end
+
+def myfunction6(a, b, c)
+  return my_wrapped_dependency(a, b, c)
+end
+
+def myfunction7(a, b, c)
+  return my_wrapped_dependency(a, b, c)
+end
+
+def myfunction8(a, b, c)
+  return my_wrapped_dependency(a, b, c)
+end
+
+def myfunction9(a, b, c)
+  return my_wrapped_dependency(a, b, c)
+end
+
+def myfunction10(a, b, c)
+  return my_wrapped_dependency(a, b, c)
+end
+
+def myfunction11(a, b, c)
+  return my_wrapped_dependency(a, b, c)
+end
+
+def myfunction1(a, b, c)
+  return my_wrapped_dependency(a, b, c)
+end
