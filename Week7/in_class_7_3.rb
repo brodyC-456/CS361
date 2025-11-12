fb4014d294771a20b046d707ce65c4db1f946262
Brody Couture <brodycouture05@gmail.com>
2025-11-12 11:46:10 -0800

    finished in class 7.3

---
+++ Week7/in_class_7_3.rb
@@ -0,0 +1,34 @@
+class MockNetConnection
+    def send(r)
+        puts "im sending " + r
+    end
+
+    def recv
+        puts "This is the data i am recieving at this moment right now"
+    end
+
+class DataManager
+  def initialize(network_connection)
+    @network_connection = network_connection
+  end
+
+  def send_request(r)
+    @network_connection.send(r)  # Send some data
+  end
+
+  def recv_response()
+    @network_connection.recv()  # Return the received data
+  end
+end
+
+fake_connection = MockNetConnection.new
+real_connection = NetworkConnection.new
+# TODO: Make DataManager behave differently if testing
+if testing
+    dm = DataManager.new(fake_connection)
+else
+    dm = DataManager.new(real_connection)
+end
+
+dm.send_request("REQUEST: good things")
+good_things = dm.recv_response()
